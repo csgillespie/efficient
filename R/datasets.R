05eb5cad5b31592afebf907be9887741203b56f1
Colin Gillespie <csgillespie@gmail.com>
2015-09-30 10:06:11 +0100

    Adding MIPs per die

--- R/datasets.R
+++ R/datasets.R
@@ -6,3 +6,15 @@
 #' @format A two column data frame
 #' @source https://csgillespie.wordpress.com/2011/01/25/cpu-and-gpu-trends-over-time/
 NULL
+
+#' @name die
+#' @title MIPS per die
+#' @description The year, clock speed and brief description of CPUs. 
+#' The clock speed is measured in million of instructions
+#' per second per die. This data set was taken from
+#' http://en.wikipedia.org/wiki/List_of_Intel_microprocessors
+#' Each CPU is the best in it's class
+#' @docType data
+#' @format A three column data frame
+#' @source https://csgillespie.wordpress.com/2011/01/25/cpu-and-gpu-trends-over-time/
+NULL
